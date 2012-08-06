43c997c1264ec7f6f4512ecc8cd00980ba26988f
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2012-08-06 20:27:45 +0000

    Move handler_example into separate file.

--- handler.js
+++ handler.js
@@ -90,23 +90,3 @@ exports.SessionStore = SessionStore;
 exports.session = session;
 exports.creds = creds;
 
-var example = function () {
-
-    var store = new SessionStore({filestore_creator: exports.local_filestore_creator});
-
-    var login_required = function (login_url) {
-	console.log('Need to log in: ' + login_url);
-    };
-
-    store.get('', function (sess) {
-	sess.synched(login_required, function (sess) {
-	    console.log('Files in /:');
-	    l = sess.get_list('/');
-	    for (idx in l) {
-		console.log(l[idx].path);
-	    }
-	});
-    });
-};
-
-example();
---
+++ handler_example.js
@@ -0,0 +1,23 @@
+
+var handler = require('./handler');
+
+var example = function () {
+
+    var store = new handler.SessionStore({filestore_creator: exports.local_filestore_creator});
+
+    var login_required = function (login_url) {
+	console.log('Need to log in: ' + login_url);
+    };
+
+    store.get('', function (sess) {
+	sess.synched(login_required, function (sess) {
+	    console.log('Files in /:');
+	    l = sess.get_list('/');
+	    for (idx in l) {
+		console.log(l[idx].path);
+	    }
+	});
+    });
+};
+
+example();
